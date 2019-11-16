c0409ca74128ff644f1820e61c6b34ddfabec7b9
mohit1325 <mohitshah13398@gmail.com>
2019-11-16 11:59:36 +0530

    fixed UI a little bit and added loading gif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ProfileWebsite';
+  loaded:boolean;
+
+
+  ngOnInit(){
+    this.loaded=false;
+    setTimeout(()=>{
+      this.loaded=true;
+    },3000);
+  }
 }
